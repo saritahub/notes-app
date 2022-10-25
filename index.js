0240aa4f732964164fe496562b2e9e4c61faf05d
Sarita <saritaradia65@gmail.com>
2022-10-25 14:03:24 +0100

    Refactor

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 console.log("The notes app is running")
 
 const NotesModel = require('./NotesModel');
+const NotesModelView = require('./NotesModelView');
 const model = new NotesModel();
-model.addNotes("Work out tonight")
+model.addNotes("Work out tonight");
+model.addNotes("Cook pasta");
 console.log(model.getNotes());
+
+const view = new NotesModelView();
+view.displayNotes(model.getNotes()); // Adds above notes to the HTML page 
